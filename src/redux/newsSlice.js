252ab519ffc517f4460e1e34cdd282f1631fb370
samson <sunnext2010@gmail.com>
2022-07-12 20:17:17 +0100

    comment slice action out

--- src/redux/newsSlice.js
+++ src/redux/newsSlice.js
@@ -1,21 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import newsService from '../services/newsService';
 
-export const getAllTasks = createAsyncThunk(
-    'users/tasks',
-    async ({ id }, { rejectWithValue }) => {
-        try {
-            const response = await newsService.getAllTasks();
-            return response;
-        } catch (err) {
-            let error = err; // cast the error for access
-            if (!error.response) {
-                throw err;
-            }
-            return rejectWithValue(error.response.data);
-        }
-    }
-);
+// export const getAllTasks = createAsyncThunk(
+//     'users/tasks',
+//     async ({ id }, { rejectWithValue }) => {
+//         try {
+//             const response = await newsService.getAllTasks();
+//             return response;
+//         } catch (err) {
+//             let error = err; // cast the error for access
+//             if (!error.response) {
+//                 throw err;
+//             }
+//             return rejectWithValue(error.response.data);
+//         }
+//     }
+// );
 
 const initialState = {
     news: {},
@@ -27,16 +27,16 @@ const newsSlice = createSlice({
     initialState: initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder.addCase(getAllTasks.fulfilled, (state, { payload }) => {
-            state.news = payload;
-        });
-        builder.addCase(getAllTasks.rejected, (state, action) => {
-            if (action.payload) {
-                state.error = action.payload.errorMessage;
-            } else {
-                state.error = action.error.message;
-            }
-        });
+        // builder.addCase(getAllTasks.fulfilled, (state, { payload }) => {
+        //     state.news = payload;
+        // });
+        // builder.addCase(getAllTasks.rejected, (state, action) => {
+        //     if (action.payload) {
+        //         state.error = action.payload.errorMessage;
+        //     } else {
+        //         state.error = action.error.message;
+        //     }
+        // });
     }
 });
 
