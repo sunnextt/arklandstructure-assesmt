2e5c6875825a41386de54d467717d1256937d1b7
samson <sunnext2010@gmail.com>
2022-07-13 21:15:00 +0100

    comment slice action out

--- src/redux/newsSlice.js
+++ src/redux/newsSlice.js
@@ -1,21 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import newsService from '../services/newsService';
-
-// export const getAllTasks = createAsyncThunk(
-//     'users/tasks',
-//     async ({ id }, { rejectWithValue }) => {
-//         try {
-//             const response = await newsService.getAllTasks();
-//             return response;
-//         } catch (err) {
-//             let error = err; // cast the error for access
-//             if (!error.response) {
-//                 throw err;
-//             }
-//             return rejectWithValue(error.response.data);
-//         }
-//     }
-// );
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     news: {},
@@ -26,18 +9,7 @@ const newsSlice = createSlice({
     name: 'Application',
     initialState: initialState,
     reducers: {},
-    extraReducers: (builder) => {
-        // builder.addCase(getAllTasks.fulfilled, (state, { payload }) => {
-        //     state.news = payload;
-        // });
-        // builder.addCase(getAllTasks.rejected, (state, action) => {
-        //     if (action.payload) {
-        //         state.error = action.payload.errorMessage;
-        //     } else {
-        //         state.error = action.error.message;
-        //     }
-        // });
-    }
+    extraReducers: (builder) => {}
 });
 
 const { reducer } = newsSlice;
